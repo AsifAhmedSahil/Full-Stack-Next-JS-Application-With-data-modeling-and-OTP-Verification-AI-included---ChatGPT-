4e73418369e507ac2a4b53669cac40cc23cf3176
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-01 12:19:38 +0600

    middleware setup

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -17,3 +17,12 @@ declare module "next-auth" {
         } & DefaultSession['user']
     }
 }
+
+declare module "next-auth/jwt"{
+    interface JWT {
+        _id?: string;
+            isVerified?: boolean;
+            isAcceptingMessages?: boolean;
+            username?:string
+    }
+}
